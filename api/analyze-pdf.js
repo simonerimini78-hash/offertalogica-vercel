e43826981568b65459f59a3a69e8d406cad51e02
simonerimini78-hash <simonerimini78@gmail.com>
2026-07-21 20:06:09 +0200

    Add files via upload

--- api/analyze-pdf.js
+++ api/analyze-pdf.js
@@ -5,7 +5,7 @@ import { extractPdfWithControlledOcr } from "../lib/pdfExtractWithOcr.js";
 import {
   applyControlledPdfAiFallback,
   applyControlledPdfAiImageFallback,
-} from "../lib/pdfAiPipelineEconomicV3.js";
+} from "../lib/pdfAiPipelineEconomicV4.js";
 import { archivePdfAnalysis } from "../lib/pdfArchive.js";
 import { runPdfReaderShadow } from "../lib/pdfReaderShadow.js";
 import { enforceRateLimit, rateLimitConfig } from "../lib/rateLimit.js";
