b785342129321aba0874d2699882306e291083b9
simonerimini78-hash <simonerimini78@gmail.com>
2026-09-11 21:49:59 +0200

    OffertaLogica Informa v0.9.0

--- public/assets/editorial-config.js
+++ public/assets/editorial-config.js
@@ -1,5 +1,5 @@
 window.OFFERTALOGICA_EDITORIAL_CONFIG = Object.freeze({
-  version: "0.8.0",
+  version: "0.9.0",
   supabaseUrl: "https://kzxdamhfmzaxonpkytcf.supabase.co",
   supabaseAnonKey: "sb_publishable_poz1xBKiXceLCFV3u_tPIg_5_-ycHcl"
 });
